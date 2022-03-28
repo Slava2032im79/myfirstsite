08a6638bed0e51c3416665e001735a8be615af3e
viacheslav <p.slava79@gmail.com>
2022-03-28 11:26:22 +0300

    Merge remote-tracking branch 'origin/master'

---
+++ myfirstsite/catalog/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from .models import Task
+
+# Create your views here.
+from django.http import HttpResponse
+
+
+def index(request):
+    tasks = Task.objects.all()
+    return render(request,'catalog/index.html', {'title':'главная страница сайта', 'tasks':tasks})
+
+
+def about(request):
+    return render(request,'catalog/about.html')
+
+
